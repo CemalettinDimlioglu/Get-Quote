41568cb64419703d9e5aaa124fd5bcf4cef59826
CMLTTN <cemaldim@gmail.com>
2023-07-27 12:39:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+
+
+  const [quotes, setQuotes] = useState("");
+
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      });
+  }; 
+
+  useEffect(() => {
+  
+  getQuote();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="quote">
+        <p>{quotes.text}</p>
+        <p>{quotes.author}</p>
+        <div className="btnContainer">
+          <button onClick={getQuote}>Get Quote</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
